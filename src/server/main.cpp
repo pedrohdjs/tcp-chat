ccbc6b0a10187a60d5ea7fb501a84eda073d4750
Pedro Souza <pedrohdjs@outlook.com>
2022-07-18 18:41:41 -0300

    Did stuff

--- src/server/main.cpp
+++ src/server/main.cpp
@@ -14,29 +14,12 @@ void handle_signal(int signal){
 }
 
 int main(){
-
-    //TODO: remover comentários na entrega
-    
-    // Socket s = Socket("127.0.0.1", 3000);
-    // s.bind();
-    // s.listen(MAX_CONNECTIONS);
-    
-    // int fd = s.accept();
-    // cout << "Conexão aceita: " << endl;
-
-    // Socket::send(fd, "Server diz oi!");
-    // cout << "Dados enviados para o cliente com sucesso!!" << endl;
-
-    // string data = Socket::receive(fd);
-    // cout << "Dados recebidos do cliente: " << data << endl;
-
     Server* s = Server::get_instance();
     s->init();
     s->run();
 
-    // TODO: Descomentar na entrega
-    // signal(SIGINT, handle_signal);
-    // signal(SIGTERM, handle_signal);
+    signal(SIGINT, handle_signal);
+    signal(SIGTERM, handle_signal);
 
     s->get_worker()->join(); //Espera a thread terminar
 
